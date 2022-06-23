b82284e33fb6faaaf556918a534565361f7284cb
Deepak Kamat <61752151+Deepak-1373@users.noreply.github.com>
2022-06-23 21:23:13 +0530

    Create count-and-say-solution.py

---
+++ count-and-say-solution.py
@@ -0,0 +1,27 @@
+def countAndSay(n):
+    return ''.join(self.nextSequence(n, ['1', 'E']))
+
+def nextSequence(n, prevSeq):
+    if n == 1:
+        return prevSeq[:-1]
+
+    nextSeq = []
+    prevDigit = prevSeq[0]
+    digitCnt = 1
+    for digit in prevSeq[1:]:
+        if digit == prevDigit:
+            digitCnt += 1
+        else:
+            # the end of a sub-sequence
+            nextSeq.extend([str(digitCnt), prevDigit])
+            prevDigit = digit
+            digitCnt = 1
+
+    # add a delimiter for the next sequence
+    nextSeq.append('E')
+
+    return self.nextSequence(n-1, nextSeq)
+  
+n = 4
+print(countAndSay(n)) # "1211"
+      
